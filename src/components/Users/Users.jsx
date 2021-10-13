1724e9c44d83b173c970578d976c901fded1a381
Elena Katasonova <elena.katasonova@concentra.co.uk>
2021-10-13 17:13:41 +0100

    54. refactoring - making ajax request from componentDidMount life cycle method

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,13 +4,10 @@ import * as axios from 'axios';
 import userImg from '../../assets/img/user.png'
 
 class Users extends React.Component {
-   constructor (props) {
-     super(props);
-
-      axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-        this.props.setUsers(response.data.items)
-      }
-      )
+   componentDidMount() {
+     axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+       this.props.setUsers(response.data.items)
+     })
    }
 
    render() {
